b6a732134e9be43fb1e14ad7a3602aff6c6987ed
MRizzo1 <0518Angie@gmail.com>
2024-05-12 19:00:54 -0400

    fix: returning indexes

--- MIS_exact.py
+++ MIS_exact.py
@@ -1,17 +1,22 @@
 import rustworkx as rx
 
 def MIS_exact(G):
+    node_indexes = list(G.node_indices())
     if G.num_nodes() <= 1:
-        return list(G.nodes())
+        return node_indexes
 
-    node_indexes = G.node_indices()
     v = node_indexes[0]
-    v_neighbors = set(G.neighbors(v))
+    v_neighbors = list(G.neighbors(v))
 
-    v_neighbors.add(v)
-    v_inc = [G.get_node_data(v)] + MIS_exact(G.subgraph(list(set(node_indexes).difference(v_neighbors))))
-    v_exc = MIS_exact(G.subgraph(list(set(node_indexes).difference([v]))))
+    _G = G.copy()
+    v_neighbors.append(v)
+    _G.remove_nodes_from(list(v_neighbors))
+    v_inc = [v] + MIS_exact(_G)
     
+    _G = G.copy()
+    _G.remove_node(v)
+    v_exc = MIS_exact(_G)
+
     if len(v_inc) > len(v_exc):
         return v_inc
     else:
