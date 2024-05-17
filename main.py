92a7951854ea68be046ed52e6fb1f52a178b15d2
lgarciausb <15-10540@usb.ve>
2024-05-17 17:04:01 -0400

    params

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
 from MIS.functions import test_benchmark
+from sys import argv
 
 if __name__ == "__main__":
-   test_benchmark(60*60)
+   try:
+      test_benchmark(int(argv[1])*60)
+   except IndexError: print(f"Usage: {argv[0]} <time>")
+   except Exception: print(f"Error: <time> must be a numeric value")
